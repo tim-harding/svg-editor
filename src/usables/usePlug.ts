5d92b4c57a9ab7a21a5dc52acb062616838ce8e0
Tim Harding <Tim@TimHarding.co>
2025-02-09 20:12:35 -0800

    Small plug refactor

--- src/usables/usePlug.ts
+++ src/usables/usePlug.ts
@@ -3,6 +3,6 @@ import { computed, ref, type Ref } from 'vue'
 export function usePlug<T>(defaultValue: T) {
   const direct = ref(defaultValue)
   const plug = ref(null as null | Ref<T | null>)
-  const value = computed(() => (plug.value === null ? direct.value : plug.value))
-  return { manualValue: direct, plug, value }
+  const resolved = computed(() => plug.value ?? direct.value)
+  return { direct, plug, resolved }
 }
