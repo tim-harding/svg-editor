44ba7a78d9cecc34d8691e4218820352a595ed20
Tim Harding <Tim@TimHarding.co>
2025-02-14 12:21:52 -0800

    Stubbing out files for node editor

---
+++ src/components/NodeEditor/NodeEditor.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { SvgTree } from '@/usables/useElement'
+import { Node } from './Node'
+
+const props = defineProps<{
+  tree: SvgTree
+}>()
+</script>
+
+<template>
+  <div>
+    <span>{{ tree.root.tag }}</span>
+  </div>
+</template>
+
+<style module="s"></style>
---
+++ src/components/NodeEditor/index.ts
@@ -0,0 +1,1 @@
+export { default as NodeEditor } from './NodeEditor.vue'
