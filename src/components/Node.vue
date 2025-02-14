e746860ffa49324f30603938af2f9d918681ffa8
Tim Harding <Tim@TimHarding.co>
2025-02-14 14:32:34 -0800

    Positioning inputs and outputs

--- src/components/Node.vue
+++ src/components/Node.vue
@@ -24,15 +24,19 @@ const transform = computed(() => {
     :class="s.node"
   >
     <span>{{ tree.root.tag }}</span>
-    <div :class="s.dropZone"></div>
+    <div :class="s.inputs"></div>
+    <div :class="s.output"></div>
   </div>
 </template>
 
 <style module="s">
 .node {
+  position: relative;
+  display: grid;
+  place-content: center;
   background-color: var(--accent-solid);
-  block-size: min-content;
-  inline-size: min-content;
+  block-size: 2.5rem;
+  inline-size: 6rem;
   padding-block: 0.25rem;
   padding-inline: 0.5rem;
   border-radius: 0.25rem;
@@ -45,9 +49,23 @@ const transform = computed(() => {
   }
 }
 
-.dropZone {
-  background-color: var(--yellow-solid);
+.inputs {
+  position: absolute;
+  top: -0.5rem;
+  left: 1rem;
+  border-radius: 0.5rem;
+  inline-size: calc(100% - 2rem);
   block-size: 1rem;
-  inline-size: 100%;
+  background-color: var(--yellow-solid);
+}
+
+.output {
+  position: absolute;
+  bottom: -0.5rem;
+  left: calc(50% - 0.5rem);
+  border-radius: 0.5rem;
+  inline-size: 1rem;
+  block-size: 1rem;
+  background-color: var(--yellow-solid);
 }
 </style>
